04b37b04e0e3d6c2252f5b5bad2f2dde003ff9cf
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-26 21:33:54 +0200

    strcuture and layout

--- src/index.js
+++ src/index.js
@@ -3,15 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import questions from './questions';
+
+function QuizeApp(props){
+  return (
+    <div className='questionaire'>
+      <div className='questionaire-header'>
+        <h1>Quiz</h1>
+      </div>
+
+      {
+        questions.map(questionObject => {
+          return (
+            <div className='question'>
+              <p>{questionObject.question}</p>
+
+              <div className='checkbox-grp'>
+                  {
+                    questionObject.answers.map(answer => {
+                      return (
+                        <div className='checkbox'>
+                          <input type="checkbox" id={answer.answer} name={answer.answer} value={answer.isCorrect} />
+                          <label for={answer.answer}>{answer.answer}</label> <br />
+                        </div>
+                      )
+                    })
+                  }
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QuizeApp />
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/questions.js
@@ -0,0 +1,89 @@
+let questions = [
+    {
+      question: "The closest planet to the sun ?",
+      answers: [
+        {
+          answer: "Venus",
+          isCorrect: false
+        },
+        {
+          answer: "Earth",
+          isCorrect: false
+        },
+        {
+          answer: "Mercury",
+          isCorrect: true
+        },
+        {
+          answer: "Saturn",
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      question: "How many continents are there ?",
+      answers: [
+        {
+          answer: "5",
+          isCorrect: false
+        },
+        {
+          answer: "2",
+          isCorrect: false
+        },
+        {
+          answer: "8",
+          isCorrect: false
+        },
+        {
+          answer: "7",
+          isCorrect: true
+        },
+      ]
+    },
+    {
+      question: "How many sides does a pentagon have",
+      answers: [
+        {
+          answer: "2",
+          isCorrect: false
+        },
+        {
+          answer: "5",
+          isCorrect: true
+        },
+        {
+          answer: "3",
+          isCorrect: false
+        },
+        {
+          answer: "8",
+          isCorrect: false
+        }
+      ]
+    },
+    {
+      question: "What is the name of the fourth book in the ‘Harry Potter’ series ?",
+      answers: [
+        {
+          answer: "Harry Potter and the Goblet of Fire",
+          isCorrect: true
+        },
+        {
+          answer: "Harry Potter and the prisoner of Azkhaban",
+          isCorrect: false
+        },
+        {
+          answer: "Harry Potter and the Half blood prince",
+          isCorrect: false
+        },
+        {
+          answer: "Harry Potter and Philosphers stone",
+          isCorrect: false
+        }
+      ]
+    },
+  ]
+
+export default questions;  
+
